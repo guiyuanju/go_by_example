57950801ceaecf89d3d67c46fbc287d779ca9de1
j30048314 <juguiyuan@huawei.com>
2024-08-15 10:16:06 +0800

    add read_file_line_by_line example

---
+++ read_file_line_by_line.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+	lines := make([]string, 0)
+
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+
+	if err := sc.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
